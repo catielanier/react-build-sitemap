e97b1e4a173b1bf2e7b5573e9680412b8d584fc7
Corey Lanier <iam@coreylanier.com>
2020-08-11 11:24:17 -0400

    preparing testing suite

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ import PropTypes from "prop-types";
 import { warn } from "console";
 import mapJson from "./src/mapJson";
 
-const buildSitemap = (fileName, buildPath) => {
-  //open component
+const buildSitemap = (fileName, buildPath, url) => {
+  const sitemapElements = [
+    '<?xml version="1.0" encoding="UTF-8?">',
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+  ];
   //if component does not exist, throw warning and skip.
   if (fileName === undefined || fileName === null) {
     throw new warn("Component does not exist to generate sitemap. Skipping.");
@@ -18,6 +21,11 @@ const buildSitemap = (fileName, buildPath) => {
   //find the 'router', 'browserrouter', or 'switch' element.
   const router = mapJson(jsxTree);
   //if the above elements exist, map through all routes.
+  if (router !== undefined) {
+    router.forEach((item) => {
+      console.log(item);
+    });
+  }
   //if does not exist, throw a warning saying it doesn't exist and skip running.
   //generate xml file string.
   //write sitemap.xml file to build path.
@@ -26,6 +34,7 @@ const buildSitemap = (fileName, buildPath) => {
 buildSitemap.propTypes = {
   fileName: PropTypes.string,
   buildPath: PropTypes.string,
+  url: PropTypes.url,
 };
 
 export default buildSitemap;
